92706d5945bc67dcd3de4189352eb8271253565e
Komal <komal@kali.linux>
2026-06-09 15:48:12 +0530

    Spread&Rest

---
+++ 4 Javascript 2month/spread & rest operator with destructuring.js
@@ -0,0 +1,143 @@
+
+// spread & rest operator 
+// di
+
+// 1. Question:
+{
+    const arr = [100, 200, 300, 400, 500, 600];
+    const [a, b, c, ...r] = arr;
+
+    console.log(a, b, c); // o/p=> 100 200 300
+    console.log(arr) // o/p=> [ 100, 200, 300, 400, 500, 600 ]
+    console.log(...r) // o/p=> 400 500 600
+    console.log([arr]) // o/p=> [ [ 100, 200, 300, 400, 500, 600 ] ]
+    console.log(...arr) // o/p=> 100 200 300 400 500 600
+}
+
+
+// 2. Question:
+{
+    const arr = [10, 20, 30, 40];
+    const [a, , b] = arr;
+
+    console.log(a, b);
+
+}
+// output=> 10 30
+
+
+
+// 3. Question:
+{
+    const user = {
+        name: "Komal",
+        age: 22
+    };
+
+    const { name, age } = user;
+
+    console.log(name, age);
+    console.log(user)
+}
+//output: Komal 22
+//output { name: 'Komal', age: 22 }
+
+// 4. Question:
+{
+    const arr1 = [1, 2];
+    const arr2 = [3, 4];
+
+    const result = [...arr1, ...arr2];
+
+    console.log(result);
+}
+// output=> [ 1, 2, 3, 4 ]
+
+// 5. Question: 
+{
+    const arr = [10, 20, 30];
+
+    console.log(...arr);
+}
+// output: 10 20 30
+
+// 6. Question:
+{
+    const user = {
+        name: "Komal"
+    };
+
+    const details = {
+        age: 22
+    };
+
+    const result = {
+        ...user,
+        ...details
+    };
+
+    console.log(result);
+}
+// output:
+// { name: 'Komal', age: 22 }
+
+
+// 7. Question:
+{
+    function demo(...nums) {
+        console.log(...nums);
+        console.log(nums)
+    }
+
+    demo(1, 2, 3, 4);
+}
+//output: 
+// 1 2 3 4
+// [ 1, 2, 3, 4 ]
+
+
+// 8.Question:
+{
+    const arr = [10, 20, 30, 40, 50];
+
+    const [first, ...rest] = arr;
+
+    console.log(first);
+    console.log(rest);
+}
+// output:
+// 10
+// [ 20, 30, 40, 50 ]
+
+// 9. Question:
+
+{
+    function show(a, b, ...rest) {
+        console.log(a);
+        console.log(b);
+        console.log(rest);
+    }
+
+    show(10, 20, 30, 40, 50);
+}
+
+//output:
+// 10
+// 20
+// [ 30, 40, 50 ]
+
+// 10. Question:
+{
+    const numbers = [1, 2, 3, 4, 5];
+
+    const [a, b, ...rest] = numbers;
+
+    console.log(a);
+    console.log(b);
+    console.log(rest);
+}
+
+// output:
+// 1
+// 2
+// [ 3, 4, 5 ]
--- 4 Javascript 2month/test.js
+++ 4 Javascript 2month/test.js
@@ -1,7 +1,37 @@
-let n = 3
-let bag = ''
-for (let i = 1; i <= n; i++) {
-    bag = bag + i + ' '
+function validname(name) {
+    return /^[A-Za-z ]+$/.test(name);
 }
 
-console.log(bag) 
+function validEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+function validPassword(password) {
+    return password.length >= 8;
+}
+
+const userdata = [
+    ['siya sharma', 'siya.sharma@gmail', 'siya@1234'],
+    ['komal kashyap', 'komal.kashyap@gmail.com', 'komal@1234'],
+    ['Ritik Roshan', 'ritik.roshan@gmail.com', 'R@1234']
+];
+
+for (let user of userdata) {
+    if (validname(user[0].trim())) {
+        console.log('validName =>', user[0].trim());
+    } else {
+        console.log('Invalid Name');
+    }
+
+    if (validEmail(user[1].trim())) {
+        console.log('validEmail =>', user[1].trim());
+    } else {
+        console.log('Invalid Email');
+    }
+
+    if (validPassword(user[2].trim())) {
+        console.log('validPassword =>', user[2].trim());
+    } else {
+        console.log('Invalid Password');
+    }
+}
